28270dd978aa7002ed8c9e239630292bb0621285
woojin126 <woojin126@naver.com>
2021-06-10 18:57:28 +0900

    Address @embeddable

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,18 @@ import javax.persistence.Embeddable;
 
 @Embeddable /*어딘가에 내장이 될수잇다는 뜻*/
 @Getter
-public class Address {
-
+public class Address {  /*값 타입은 setter를 제공안하고 생성할때만 */
     private String city;
     private String street;
     private String zipcode;
+
+
+    protected Address() {
+
+    }
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
