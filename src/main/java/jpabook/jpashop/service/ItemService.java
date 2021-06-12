e9953d3bd4a4d89e658ef0384a54c9b6874c5ed5
woojin126 <woojin126@naver.com>
2021-06-12 16:36:48 +0900

    ItemService

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional//이걸안쓰면 위에 전체값이 리드온리라 저장이안됨 == false 라는것
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    //조회
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
