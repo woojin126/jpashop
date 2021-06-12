f104efbcfaea5d4c1b42e45c7b2e77b4818085a7
woojin126 <woojin126@naver.com>
2021-06-12 16:34:21 +0900

    homeController

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+/*최근에 검색했던 리소스 컨트롤+E*/
+    @RequestMapping("/")
+    public String home(){
+        log.info("home Controller");
+        return "home";
+    }
+}
