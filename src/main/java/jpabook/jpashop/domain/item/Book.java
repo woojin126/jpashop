cd9047ae583ccb0f31992b01246fd5574b466e1b
woojin126 <woojin126@naver.com>
2021-06-12 16:31:32 +0900

    Book DTO

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -16,4 +16,18 @@ public class Book extends Item{
     private String author;
     private String isbn;
 
+
+    public static Book createBookInfo(Long id,String name,int price,int stockQuantity,String author,String isbn){
+
+        Book book = new Book();
+        book.setId(id);
+        book.setName(name);
+        book.setPrice(price);
+        book.setAuthor(author);
+        book.setStockQuantity(stockQuantity);
+        book.setIsbn(isbn);
+
+        return book;
+
+    }
 }
