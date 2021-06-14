e1f9dcc5678b41513aa64706f62ae9ecf12f8138
woojin126 <woojin126@naver.com>
2021-06-14 17:01:24 +0900

    itemService  아이템 수정 로직

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional//이걸안쓰면 위에 전체값이 리드온리라 저장이안됨 == false 라는것
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    /*Long itemId,*/
+    @Transactional
+    public void updateItem(Book book){
+        Item findItem = itemRepository.findOne(book.getId());
+        findItem.setName(book.getName());
+        findItem.setPrice(book.getPrice());
+        findItem.setStockQuantity(book.getStockQuantity());
+        /*이런식으로 set으로 하기보다는 의미있는 메서드를 만들어서 사용하자*/
     }
 
     //조회
